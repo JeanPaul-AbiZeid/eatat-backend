e27ad6b93c227e3630c16d8d950f838d68ca1e38
jean paul <jeanpaulabizeid@gmail.com>
2022-05-28 19:57:53 +0300

    creating connection to local host

---
+++ connection.php
@@ -0,0 +1,11 @@
+<?php
+
+$host = "localhost";
+$db_user = "root"; //should be changed
+$db_pass = null;  //should be changed
+$db_name = "eatatdb";
+
+$mysqli = new mysqli($host, $db_user, $db_pass, $db_name);
+
+
+?>
