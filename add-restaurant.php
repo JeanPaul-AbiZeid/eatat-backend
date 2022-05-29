3673c81cdb1eca582bccfcbdf4b9f87761791781
jean paul <jeanpaulabizeid@gmail.com>
2022-05-29 18:02:57 +0300

    creating variables

--- add-restaurant.php
+++ add-restaurant.php
@@ -1,4 +1,10 @@
 <?php
 include("connection.php");
 
+$name = $_POST["name"];
+$description =$_POST["description"];
+$image = $_POST["image"];
+$average_price = $_POST["avg_cost"];
+$location = $_POST["location"];
+$category = $_POST["category"];
 ?>
