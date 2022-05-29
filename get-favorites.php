5600140ba1695a8121088edaa360b82683d7644e
jean paul <jeanpaulabizeid@gmail.com>
2022-05-29 18:38:44 +0300

    executing query to get users' favorites

--- get-favorites.php
+++ get-favorites.php
@@ -1,4 +1,14 @@
 <?php
 include("connection.php");
 
+$user_id = $_GET["user_id"];
+
+$query = $mysqli->prepare("select * from restaurants join favorites on favorites.restaurant_id = restaurants.id where favorites.user_id = ?");
+$query->bind_param("s", $user_id);
+$query->execute();
+
+$array = $query->get_result();
+$num_rows = $array->num_rows;
+
+$result = [];
 ?>
