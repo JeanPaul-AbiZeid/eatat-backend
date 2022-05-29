35c6bf43f93ef2a31b1aee47200a572806a006ac
Micho-PC <michelhdd27@gmail.com>
2022-05-29 16:57:08 +0300

    updated sign up

--- sign-up.php
+++ sign-up.php
@@ -1,20 +1,31 @@
 <?php
-//header('Access-Control-Allow-Origin: *'); this removes the error but code still works without it
+header('Access-Control-Allow-Origin: *'); //this removes the error 
 include("connection.php");
 
 $first_name = $_POST["first-name"];
 $last_name = $_POST["last-name"];
 $email = $_POST["email"];
-$password = hash("sha256", $_POST["pass"]);
+$password = hash("sha256", $_POST["password"]);
 
-$query = $mysqli->prepare("INSERT INTO users(first_name, last_name, email, password) VALUES (?, ?, ?, ?)");
-$query->bind_param("ssss", $first_name, $last_name,$email,$password); //this line helps avoid sql injections from hackers
+$query = $mysqli->prepare("select email from users where email=?");
+$query->bind_param("s",$email);
 $query->execute();
+
+$query->store_result();
+$rows = $query->num_rows;
 
 $response = [];
 
-$response["success"] = true;
+if($rows >0){
+    $response["response"] = "Email already exists";
+}else{
+    $query = $mysqli->prepare("INSERT INTO users(first_name, last_name, email, password,type) VALUES (?, ?, ?, ?, 1)");
+    $query->bind_param("ssss", $first_name, $last_name,$email,$password); //this line helps avoid sql injections from hackers
+    $query->execute();
+    $response["success"] = true;
+}
 
 echo json_encode($response);
+
 
 ?>
