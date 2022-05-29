39c91dbcbe406e6509ed707a91fba2707da8b34d
jean paul <jeanpaulabizeid@gmail.com>
2022-05-29 18:05:11 +0300

    checking for restaurant and creating response

--- add-restaurant.php
+++ add-restaurant.php
@@ -14,4 +14,21 @@ $query->execute();
 
 $query->store_result();
 $num_rows = $query->num_rows;
+
+$response = [];
+
+if ($num_rows > 0) {
+    $response["response"] = "Restaurant already exists";
+}else{
+    $query = $mysqli->prepare("INSERT INTO restaurants(name, description, image, avg_cost, location, category) VALUES(?, ?, ?, ?, ?, ?)");
+    $query->bind_param("ssssss", $name, $description, $image, $average_price, $location, $category);
+    $query->execute();
+
+    $response["success"] = true;
+}
+
+
+header('Access-control-Allow-Origin:*');
+$json = json_encode($response);
+echo $json;
 ?>
