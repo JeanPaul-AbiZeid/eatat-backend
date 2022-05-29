485a05507555309e632eff87e5cf998cc249ce7c
jean paul <jeanpaulabizeid@gmail.com>
2022-05-29 18:01:01 +0300

    creating json response to make a restaurant as favorite

--- add-favorites.php
+++ add-favorites.php
@@ -1,6 +1,19 @@
 <?php
 include("connection.php");
 
+$user_id = $_GET["user_id"];
+$restaurant_id = $GET["restaurant_id"]
 
+$query = $mysqli->prepare("INSERT INTO favorites VALUES(?, ?)");
+$query->bind_param("ss", $user_id, $restaurant_id);
+$query->execute();
+
+
+$response = [];
+$response["success"] = true;
+
+header('Access-control-Allow-Origin:*');
+$json = json_encode($response);
+echo $json;
 
 ?>
