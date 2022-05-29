57ecc5e821edaac238b1f0818f8d2038a34ab760
Micho-PC <michelhdd27@gmail.com>
2022-05-29 13:48:35 +0300

    fixed sign up api

--- sign-up.php
+++ sign-up.php
@@ -8,10 +8,11 @@ $email = $_POST["email"];
 $password = hash("sha256", $_POST["pass"]);
 
 $query = $mysqli->prepare("INSERT INTO users(first_name, last_name, email, password) VALUES (?, ?, ?, ?)");
-$query->bind_params("ssii", $first_name, $last_name,$email,$password); //this line helps avoid sql injections from hackers
+$query->bind_param("ssss", $first_name, $last_name,$email,$password); //this line helps avoid sql injections from hackers
 $query->execute();
 
 $response = [];
+
 $response["success"] = true;
 
 echo json_encode($response);
