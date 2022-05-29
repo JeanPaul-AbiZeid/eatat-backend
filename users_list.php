35681d3e28a4780bec56948cd61334c71954c2dd
jean paul <jeanpaulabizeid@gmail.com>
2022-05-29 17:48:10 +0300

    creating json response to list all the users

--- users_list.php
+++ users_list.php
@@ -1,6 +1,18 @@
 <?php
 include("connection.php");
 
+$query = $mysqli->prepare("SELECT id, first_name, last_name email from users");
+$query->execute();
 
+$array = $query->get_result();
+$response = [];
+
+while($users = $array->fetch_assoc()){
+    $response[] = $users;
+}
+
+header('Access-control-Allow-Origin:*');
+$json = json_encode($response);
+echo $json;
 
 ?>
