57dd24a4de3c1a3a333b7338c086f3050aa2385d
jean paul <jeanpaulabizeid@gmail.com>
2022-05-29 17:50:55 +0300

    creating json response to list the restaurants

--- explore.php
+++ explore.php
@@ -1,6 +1,16 @@
 <?php
-//done
 include("connection.php");
+$query = $mysqli->prepare("SELECT * from restaurants");
+$query->execute();
 
+$array = $query->get_result();
+$response = [];
+
+while($restaurant = $array->fetch_assoc()){
+    $response[] = $restaurant;
+}
+header('Access-control-Allow-Origin:*');
+$json = json_encode($response);
+echo $json;
 
 ?>
