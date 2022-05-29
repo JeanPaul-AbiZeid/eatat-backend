506539495db93edbd919d4c8176ffff12f367c1c
jean paul <jeanpaulabizeid@gmail.com>
2022-05-29 18:03:45 +0300

    executing query to get the name

--- add-restaurant.php
+++ add-restaurant.php
@@ -7,4 +7,11 @@ $image = $_POST["image"];
 $average_price = $_POST["avg_cost"];
 $location = $_POST["location"];
 $category = $_POST["category"];
+
+$query = $mysqli->prepare("select name from restaurants where name = ?");
+$query->bind_param("s", $name);
+$query->execute();
+
+$query->store_result();
+$num_rows = $query->num_rows;
 ?>
