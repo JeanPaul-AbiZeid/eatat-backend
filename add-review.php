f29813cf891b3adbf00cb10aa31a666ee621cb88
jean paul <jeanpaulabizeid@gmail.com>
2022-05-30 09:39:37 +0300

    creating php file to get the avg ratings

--- add-review.php
+++ add-review.php
@@ -3,8 +3,8 @@ include("connection.php");
 
 $user_id = $_POST["user_id"];
 $restaurant_id = $_POST["restaurant_id"];
-$review =  $_GET["review"];
-$ratings = $_GET["ratings"];
+$review =  $_POST["review"];
+$ratings = $_POST["ratings"];
 
 $query = $mysqli->prepare("INSERT INTO reviews(review, ratings, user_id, restaurant_id, is_pending) VALUES(?, ?, ?, ?, 1)");
 $query->bind_param("ssss", $review, $ratings, $user_id, $restaurant_id);
