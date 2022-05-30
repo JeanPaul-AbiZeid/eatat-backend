55a1484e6fb49884446c351fae3dc40e6cdb7f1e
jean paul <jeanpaulabizeid@gmail.com>
2022-05-30 22:12:39 +0300

    creating api to remove from favorites

---
+++ remove-favorite.php
@@ -0,0 +1,25 @@
+<?php
+include("connection.php");
+
+if(isset($_POST["user_id"])){
+    $user_id = $_POST["user_id"];
+}else{
+    die("missing user id");
+}
+
+if(isset($_POST["restaurant_id"])){
+    $restaurant_id = $_POST["restaurant_id"];
+}else{
+    die("missing restaurant id");
+}
+
+$query = $mysqli->prepare("delete from favorites where user_id = ? AND restaurant_id = ?");
+$query->bind_param("ss", $user_id, $restaurant_id);
+$query->execute();
+
+$response = [];
+$response["success"] = true;
+
+$json = json_encode($response);
+echo $json;
+?>
