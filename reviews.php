a97ede70e649e3d9a1da41f92a151764ac3a085f
jean paul <jeanpaulabizeid@gmail.com>
2022-05-30 18:38:34 +0300

    fixing query to get review items

--- reviews.php
+++ reviews.php
@@ -1,8 +1,7 @@
 <?php
 include("connection.php");
 
-$query = $mysqli->prepare("SELECT * from reviews");
-// $query->bind_param("s", $restaurant_id);
+$query = $mysqli->prepare("SELECT first_name, last_name, picture, review, ratings, name, is_pending from reviews join users on users.id = reviews.user_id join restaurants on restaurants.id = reviews.restaurant_id");
 $query->execute();
 
 $array = $query->get_result();
