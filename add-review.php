7a0148280c8d10e488f4437124745bd241bc077c
jean paul <jeanpaulabizeid@gmail.com>
2022-05-30 09:09:12 +0300

    creating row in the reviews table

--- add-review.php
+++ add-review.php
@@ -6,6 +6,15 @@ $restaurant_id = $_POST["restaurant_id"];
 $review =  $_GET["review"];
 $ratings = $_GET["ratings"];
 
+$query = $mysqli->prepare("INSERT INTO reviews(review, ratings, user_id, restaurant_id, is_pending) VALUES(?, ?, ?, ?, 1)");
+$query->bind_param("ssss", $review, $ratings, $user_id, $restaurant_id);
+$query->execute();
 
+$response = [];
+$response["success"] = true;
+
+header('Access-control-Allow-Origin:*');
+$json = json_encode($response);
+echo $json;
 
 ?>
