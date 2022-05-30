49dfd228a5e15495c14c1d9bd20eef7f9c6fb876
jean paul <jeanpaulabizeid@gmail.com>
2022-05-30 21:23:10 +0300

    creating delete php file to remove user from database

---
+++ delete-user.php
@@ -0,0 +1,21 @@
+<?php
+include("connection.php");
+
+
+if(isset($_POST["user_id"])){
+    $user_id = $_POST["user_id"];
+}else{
+    die("missing user id");
+}
+
+$query = $mysqli->prepare("delete from users where id = ?");
+$query->bind_param("s", $user_id);
+$query->execute();
+
+$response = [];
+$response["success"] = true;
+
+$json = json_encode($response);
+echo $json;
+
+?>
