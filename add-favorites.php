016a5ab08519f148e7e3f8ec10b707d8fe120d1a
jean paul <jeanpaulabizeid@gmail.com>
2022-05-30 09:11:06 +0300

    fixing query to insert favorites

--- add-favorites.php
+++ add-favorites.php
@@ -4,7 +4,7 @@ include("connection.php");
 $user_id = $_POST["user_id"];
 $restaurant_id = $_POST["restaurant_id"];
 
-$query = $mysqli->prepare("INSERT INTO favorites VALUES(?, ?)");
+$query = $mysqli->prepare("INSERT INTO favorites(user_id, restaurant_id) VALUES(?, ?)");
 $query->bind_param("ss", $user_id, $restaurant_id);
 $query->execute();
 
