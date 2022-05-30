23dcf715063959974fd7f8c84558ecb22c0b69fc
jean paul <jeanpaulabizeid@gmail.com>
2022-05-30 09:26:54 +0300

    changing pending status to 0

--- confirm-review.php
+++ confirm-review.php
@@ -1,6 +1,18 @@
 <?php
 include("connection.php");
 
+$user_id = $_POST["user_id"];
+$restaurant_id = $_POST["restaurant_id"];
 
+$query = $mysqli->prepare("update reviews set is_pending = '0' where user_id = ? AND restaurant_id = ?");
+$query->bind_param("ss", $user_id, $restaurant_id);
+$query->execute();
+
+$response = [];
+$response["success"] = true;
+
+header('Access-control-Allow-Origin:*');
+$json = json_encode($response);
+echo $json;
 
 ?>
