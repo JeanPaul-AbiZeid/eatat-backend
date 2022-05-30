b58b97d339f3c62310a1b7a86b3a1e7f1ae452f9
jean paul <jeanpaulabizeid@gmail.com>
2022-05-30 10:01:43 +0300

    creating json response to display the reviews

--- restaurant-review.php
+++ restaurant-review.php
@@ -3,5 +3,18 @@ include("connection.php");
 
 $restaurant_id = $_GET["restaurant_id"];
 
+$query = $mysqli->prepare("select review from reviews where restaurant_id = ?");
+$query->bind_param("s", $restaurant_id);
+$query->execute();
 
+$array = $query->get_result();
+$num_rows = $array->num_rows;
+
+$response = [];
+$restaurant = $array->fetch_assoc();
+$response = $restaurant;
+
+header('Access-control-Allow-Origin:*');
+$json = json_encode($response);
+echo $json;
 ?>
