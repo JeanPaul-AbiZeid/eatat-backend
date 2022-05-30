de2264cb56722475d84044991c77313570a27d3d
jean paul <jeanpaulabizeid@gmail.com>
2022-05-30 09:48:14 +0300

    creating query to get the average ratings of a restaurant

--- avg-ratings.php
+++ avg-ratings.php
@@ -1,5 +1,20 @@
 <?php
 include("connection.php");
 
+$restaurant_id = $_GET["restaurant_id"];
 
+$query = $mysqli->prepare("select avg(ratings) from reviews where restaurant_id = ?");
+$query->bind_param("s", $restaurant_id);
+$query->execute();
+
+$array = $query->get_result();
+$num_rows = $array->num_rows;
+
+$response = [];
+$restaurant = $array->fetch_assoc();
+$response = $restaurant;
+
+header('Access-control-Allow-Origin:*');
+$json = json_encode($response);
+echo $json;
 ?>
